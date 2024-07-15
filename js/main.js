40a1c9a8e7fa8622a284056a7f741dfabab857b3
Sho Matsuzaki <sho.g.matsuzaki@gmail.com>
2024-07-14 22:01:54 -0400

    Added content for homepage for all device sizes, disabled zoom animation temporarily

--- js/main.js
+++ js/main.js
@@ -7,5 +7,28 @@ let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth |
 let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
 ////////////////////////////////////////////////////////////////////////////////
+//                                HOMEPAGE INFO                               //
+////////////////////////////////////////////////////////////////////////////////
+
+function displayTime() {
+	// get current date and time
+	var now = new Date();
+	const options = {
+		weekday: "short",
+		year: "numeric",
+		month: "short",
+		day: "numeric"
+	};
+	var dateOnly = now.toLocaleDateString(undefined, options);
+	var timeOnly = now.toLocaleTimeString();
+
+	//insert date and time into HTML
+	document.getElementById("date-time").innerHTML = dateOnly + ", " + timeOnly;
+}
+
+setInterval(displayTime, 1000); // update every second
+
+
+////////////////////////////////////////////////////////////////////////////////
 //                           WINDOW EVENT LISTENERS                           //
 ////////////////////////////////////////////////////////////////////////////////
