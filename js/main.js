d637b08a84d9dcd400e7ec96a98379a75c8f31b7
Sho Matsuzaki <sho.g.matsuzaki@gmail.com>
2024-07-16 21:33:49 -0400

    Added transition for homepage info and resizing logic for grid

--- js/main.js
+++ js/main.js
@@ -2,12 +2,19 @@
 //                                   GENERAL                                  //
 ////////////////////////////////////////////////////////////////////////////////
 
+// get elements that need to be targeted repeatedly
+let grid = document.getElementById("grid-logo");
+let homepageInfo = document.getElementById("homepage-info");
+
 // get viewport width and height
 let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-// boolean for whether or not page is fully loaded
+// boolean for whether or not load animation is done
 let isLoaded = false;
+
+// boolean for whether or not we're in project view and scroll animation is done
+let inProjectView = false;
 
 // cubic ease-in function, takes in and outputs a value from 0 to 1
 function easeInCubic(x) {
@@ -29,12 +36,62 @@ window.onresize = function() {
 	// update viewport width and height
 	vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 	vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+	// resize grid based on where we are in user flow
+	if (!isLoaded) {
+		if (vw > 900) {
+			// desktop
+			grid.style.height = (3.5*vw) + "px";
+			grid.style.top = (3.5*vw/-2 + vh/2) + "px";
+			grid.style.left = "0px";
+			grid.style.strokeWidth = "1px";
+		} else {
+			// tablet and mobile
+			grid.style.height = (3.5*vw*38/32) + "px";
+			grid.style.top = (3.5*vw*38/32/-2 + vh/2) + "px";
+			grid.style.left = ((vw*38/32)/-2 + vw/2) + "px";
+			grid.style.strokeWidth = "1px";
+		}
+	} else if (isLoaded && !inProjectView) {
+		if (vw > 900) {
+			// desktop
+			grid.style.height = (3.5*vw*38/8) + "px";
+			grid.style.top = (3.5*vw*38/8/-2 + vh/2) + "px";
+			grid.style.left = ((vw*38/8)/-2 + vw/2) + "px";
+			grid.style.strokeWidth = "2px";
+		} else {
+			// tablet and mobile
+			grid.style.height = (3.5*vw*38/32) + "px";
+			grid.style.top = (3.5*vw*38/32/-2 + 2.5*1/32*vw) + "px";
+			grid.style.left = ((vw*38/32)/-2 + vw/2) + "px";
+			grid.style.strokeWidth = "1px";
+		}
+	} else {
+		if (vw > 900) {
+			// desktop
+			grid.style.height = (3.5*vw*38/6) + "px";
+			grid.style.top = (3.5*vw*38/6/-2 + 1.5*1/6*vw) + "px";
+			grid.style.left = ((vw*38/6)/-2 + vw/2) + "px";
+			grid.style.strokeWidth = "2px";
+		} else if (vw > 600) {
+			// tablet
+			grid.style.height = (3.5*vw*38/4) + "px";
+			grid.style.top = (3.5*vw*38/4/-2 + 1.5*1/4*vw) + "px";
+			grid.style.left = ((vw*38/4)/-2 + vw/2) + "px";
+			grid.style.strokeWidth = "2px";
+		} else {
+			// mobile
+			grid.style.height = (3.5*vw*38/2) + "px";
+			grid.style.top = (3.5*vw*38/2/-2 + 1.5*1/2*vw) + "px";
+			grid.style.left = ((vw*38/2)/-2 + vw/2) + "px";
+			grid.style.strokeWidth = "2px";
+		}
+	}
 };
 
 document.addEventListener("DOMContentLoaded", () => {
 	percentLoad();
 	animateLogo();
-	animateGrid();
 	displayTime();
 	window.scrollTo(0,0);
 });
@@ -92,6 +149,7 @@ function animateLogo() {
 	var handler = setInterval(function() {
 		// stop animating once 3rd set of lines has finished animating
 		if (frame3 > totalFrames) {
+			animateGrid();
 			clearInterval(handler);
 		}
 		// first set of lines animates in by masks moving
@@ -126,11 +184,9 @@ function animateLogo() {
 }
 
 function animateGrid() {
-	var grid = document.getElementById("grid-logo"),
-		fps = 60,
+	var fps = 60,
 		duration = 2, // seconds
-		delay = 4, // seconds
-		frame = -1*delay*fps, // start frame count at negative value for delay
+		frame = 0,
 		totalFrames = duration*fps,
 		deskStartHeight = 3.5*vw, // grid has 38 columns and is 3.5x longer than it is wide, start with all 38 columns in view
 		deskEndHeight = 3.5*vw*38/8, // zoomed so only 8 columns are in vew
@@ -144,7 +200,8 @@ function animateGrid() {
 	var handler = setInterval(function() {
 		if (frame > totalFrames) {
 			//make homepage info visible
-			document.getElementById("homepage-info").style.opacity = "1";
+			homepageInfo.style.opacity = "1";
+			homepageInfo.style.filter = "blur(0px)";
 			//stop animation
 			clearInterval(handler);
 		} else if (frame >= 0) {
@@ -194,34 +251,85 @@ function displayTime() {
 // boolean for whether or not scroll animation has happened before
 let firstTime = true;
 
-function projectView() {
+// boolean for which direction user is scrolling, true is down
+let scrollDirection = true;
+
+// value for checking which direction user is scrolling
+let lastScrollVal = 0;
+
+// function to blend two hex code colors by an amount from 0 to 1
+function blendColors(colorA, colorB, amount) {
+	const [rA, gA, bA] = colorA.match(/\w\w/g).map((c) => parseInt(c, 16));
+	const [rB, gB, bB] = colorB.match(/\w\w/g).map((c) => parseInt(c, 16));
+	const r = Math.round(rA + (rB - rA) * amount).toString(16).padStart(2, '0');
+	const g = Math.round(gA + (gB - gA) * amount).toString(16).padStart(2, '0');
+	const b = Math.round(bA + (bB - bA) * amount).toString(16).padStart(2, '0');
+	return '#' + r + g + b;
+}
+
+function scrollAnimation() {
 	// pageYOffset goes from 0 to full document height minus viewport height
 	// this sets the scroll value to a decimal between 0 and 1, with 1 being full scroll
-	var scrollVal = window.pageYOffset / (document.body.offsetHeight - vh),
-		grid = document.getElementById("grid-logo"),
-		deskStartHeight = 3.5*vw*38/8, // zoomed so 8 of 38 columns in view
-		deskEndHeight = 3.5*vw*38/6, // zoomed so 6 of 38 columns in view
-		tabStartHeight = 3.5*vw*38/32, // zoomed so 32 of 38 columns in view
-		tabEndHeight = 3.5*vw*38/4, // zoomed so 4 of 38 columns in view
-		mobStartHeight = tabStartHeight,
-		mobEndHeight = 3.5*vw*38/2, // zoomed so 2 of 38 columns in view
-		deskStartTop = deskStartHeight/-2 + vh/2, // center grid
-		deskEndTop = deskEndHeight/-2 + 1.5*1/6*vw, // center grid to top of view, then down 1.5 squares
-		tabStartTop = tabStartHeight/-2 + 2.5*1/32*vw,
-		tabEndTop = tabEndHeight/-2 + 1.5*1/4*vw,
-		mobStartTop = tabStartTop,
-		mobEndTop = mobEndHeight/-2 + 1.5*1/2*vw,
-		deskStartLeft = (vw*38/8)/-2 + vw/2, // for grid to be centered, left must be negative half of grid width (38/8x of width) + half of view
-		deskEndLeft = (vw*38/6)/-2 + vw/2,
-		tabStartLeft = (vw*38/32)/-2 + vw/2,
-		tabEndLeft = (vw*38/4)/-2 + vw/2,
-		mobStartLeft = tabStartLeft,
-		mobEndLeft = (vw*38/2)/-2 + vw/2;
+	var scrollVal = window.pageYOffset / (document.body.offsetHeight - vh);
+
+	// check scroll direction
+	if (scrollVal >= lastScrollVal) {
+		scrollDirection = true; // scrolling down
+	} else {
+		scrollDirection = false; // scrolling up
+	}
+	lastScrollVal = scrollVal <= 0 ? 0 : scrollVal;
+
+	// only run if preloader animation is done
 	if (isLoaded) {
+		var gridLines = document.getElementsByClassName("grid-line"),
+			logoLines = document.getElementsByClassName("logo-line"),
+			deskStartHeight = 3.5*vw*38/8, // zoomed so 8 of 38 columns in view
+			deskEndHeight = 3.5*vw*38/6, // zoomed so 6 of 38 columns in view
+			tabStartHeight = 3.5*vw*38/32, // zoomed so 32 of 38 columns in view
+			tabEndHeight = 3.5*vw*38/4, // zoomed so 4 of 38 columns in view
+			mobStartHeight = tabStartHeight,
+			mobEndHeight = 3.5*vw*38/2, // zoomed so 2 of 38 columns in view
+			deskStartTop = deskStartHeight/-2 + vh/2, // center grid
+			deskEndTop = deskEndHeight/-2 + 1.5*1/6*vw, // center grid to top of view, then down 1.5 squares
+			tabStartTop = tabStartHeight/-2 + 2.5*1/32*vw,
+			tabEndTop = tabEndHeight/-2 + 1.5*1/4*vw,
+			mobStartTop = tabStartTop,
+			mobEndTop = mobEndHeight/-2 + 1.5*1/2*vw,
+			deskStartLeft = (vw*38/8)/-2 + vw/2, // for grid to be centered, left must be negative half of grid width (38/8x of width) + half of view
+			deskEndLeft = (vw*38/6)/-2 + vw/2,
+			tabStartLeft = (vw*38/32)/-2 + vw/2,
+			tabEndLeft = (vw*38/4)/-2 + vw/2,
+			mobStartLeft = tabStartLeft,
+			mobEndLeft = (vw*38/2)/-2 + vw/2,
+			startColor = getComputedStyle(gridLines[0]).getPropertyValue("--light-grid"),
+			endColor = getComputedStyle(logoLines[0]).getPropertyValue("--dark-grid");
+		
+		// set scroll position to 0 so initial animation is smooth
 		if (firstTime) {
 			// move scroll to the very top while doc is not fully loaded
 			window.scrollTo(0,0);
 			firstTime = false;
+		}
+		if (scrollVal > 0 && scrollDirection) {
+			// make homepage info disappear
+			homepageInfo.style.opacity = "0";
+			homepageInfo.style.filter = "blur(4px)";
+			if (scrollVal == 1) {
+				inProjectView = true;
+			}
+		}
+		else if (scrollVal < 0.2 && !scrollDirection) {
+			// bring homepage info back
+			homepageInfo.style.opacity = "1";
+			homepageInfo.style.filter = "blur(0px)";
+			if (scrollVal == 0) {
+				inProjectView = false;
+			}
+		}
+		// change grid stroke color
+		for (var i = 0; i < gridLines.length; i++) {
+		   gridLines[i].style.stroke = blendColors(startColor, endColor, scrollVal);
 		}
 		// desktop animation
 		if (vw > 900) {
@@ -244,13 +352,13 @@ function projectView() {
 			grid.style.left = (mobStartLeft + ((mobEndLeft - mobStartLeft) * scrollVal)) + "px";
 		}
 	} else {
-		// move scroll to the very top while doc is not fully loaded
+		// move scroll to the very top if preloader animation is not done and user scrolls
 		window.scrollTo(0,0);
 	}
 }
 
 window.addEventListener("scroll", () => {
-	projectView();
+	scrollAnimation();
 });
 
 
